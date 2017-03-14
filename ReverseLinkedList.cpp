31e46739d0283986e4304223a9a0a7f8388d9e42
wzvoid <wzd5211@gmail.com>
2017-03-14 18:39:59 +0800

    wzvoid

--- ReverseLinkedList.cpp
+++ ReverseLinkedList.cpp
@@ -11,24 +11,42 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
+//ListNode* reverseList(ListNode* head) {
+//    if(head){
+//        ListNode *reverse_list = NULL;
+//        ListNode *pCurrent = head, *pNext;
+//        while (pCurrent) {
+//            pNext = pCurrent->next;
+//            if(!reverse_list){
+//                pCurrent->next=NULL;
+//                reverse_list=pCurrent;
+//            }else {
+//                pCurrent->next = reverse_list;
+//                reverse_list = pCurrent;
+//            }
+//            pCurrent = pNext;
+//        }
+//        return reverse_list;
+//    }
+//    return NULL;
+//}
 ListNode* reverseList(ListNode* head) {
-    if(head){
-        ListNode *reverse_list=NULL;
-        ListNode *pCurrent=head,*pNext;
-        while(pCurrent) {
-            pNext=pCurrent->next;
-            pCurrent=reverse_list->next;
-            reverse_list=pCurrent;
-            pCurrent=pNext;
-        }
+    if(head->next){
+        ListNode *temp;
+        temp=head->next;
+        head->next=NULL;
+        reverseList(temp)->next=head;
+    }else{
+        return head;
     }
 }
 ListNode* createList(){
     ListNode* head=NULL,*p=head;
     for(int i=0;i<10;++i){
-        ListNode *temp;
-        temp->val=i;
-        temp->next=NULL;
+//        ListNode *temp=(ListNode *)malloc(sizeof(ListNode));
+//        temp->val=i;
+//        temp->next=NULL;
+        ListNode *temp = new ListNode(i);
         if(!head){
             head=p=temp;
         }
@@ -39,13 +57,24 @@ ListNode* createList(){
     }
     return head;
 }
-void show(ListNode * head){
+//顺序输出链表
+void show(ListNode *head){
     while (head){
         cout<<head->val<<" ";
         head=head->next;
     }
 }
+//递归倒序输出链表
+void recursion(ListNode *head){
+    if(head) {
+        recursion(head->next);
+        cout<<head->val;
+    }
+}
 int main() {
-    show(createList());
+    ListNode *h1,*h2;
+    h1=createList();
+    h2=reverseList(h1);
+    show(h2);
     return 0;
 }
---
+++ Unities.h
@@ -0,0 +1,8 @@
+//
+// Created by wzvoid on 2017/3/14.
+//
+
+#ifndef ALGORITHM_UNITIES_H
+#define ALGORITHM_UNITIES_H
+
+#endif //ALGORITHM_UNITIES_H
