e2e01b72124852a1d02ccec449b3d2f45b860317
wzvoid <wzd5211@gmail.com>
2017-03-17 16:15:37 +0800

    wzvoid

--- Index.cpp
+++ Index.cpp
@@ -2,12 +2,32 @@
 #include <vector>
 #include <string>
 #include <cassert>
+
 using namespace std;
+
+void f();
+void f(int a);
+void f(const int a);
+void f(int a,int b);
+void f(double a, double b);
+
 int main(int argc, char *argv[]) {
-    vector<int> v(10,1);
-    assert(v.size()>10);
-#ifndef NDEBUG
-    cerr<<__FILE__<<" "<<__FUNCTION__<<" ";
-#endif
+    f(3.3,5.7);
     return 0;
+}
+
+void f(){
+    cout<<"f()";
+}
+void f(int a){
+    cout<<"f(int a)";
+}
+void f(const int a){
+    cout<<"f(const int a)";
+}
+void f(int a,int b){
+    cout<<"f(int a,int b)";
+}
+void f(double a, double b){
+    cout<<"f(double a,double b)";
 }
