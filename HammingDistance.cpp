bf9950537c91f464068ef5b3e2fcd8993fdd4959
wzvoid <wzd5211@gmail.com>
2017-03-28 14:28:34 +0800

    进击的水母君

--- HammingDistance.cpp
+++ HammingDistance.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
 
 using namespace std;
 
-
-int hamming(int);
-
 int main() {
-    cout<<hamming(2);//  0010 0101
-    return 0;
-}
 
-int hamming(int x) {
-    unsigned int a = x;
-    return ~a;
+    return 0;
 }
