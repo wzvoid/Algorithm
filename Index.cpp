4656e5cae422665a72d6174a1e3d5aa5057f835c
wzvoid <wzd5211@gmail.com>
2019-07-28 11:29:17 +0800

    wz

--- Index.cpp
+++ Index.cpp
@@ -8,31 +8,44 @@ struct TreeNode {
     TreeNode *left;
     TreeNode *right;
 
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 
-void create(TreeNode *&root) {
-    int a;
-    cin >> a;
-    if (a == 0) {
-        root = NULL;
-    } else {
-        root = new TreeNode(a);
-        create(root->left);
-        create(root->right);
+void createBiTree1(TreeNode *&root) {
+    int val;
+    cin >> val;
+    if (val == 0)
+        root = nullptr;
+    else {
+        root = new TreeNode(val);
+        createBiTree1(root->left);
+        createBiTree1(root->right);
+    }
+}
+
+TreeNode *createBiTree2() {
+    int val;
+    cin >> val;
+    if (val == 0)
+        return nullptr;
+    else {
+        auto *root = new TreeNode(val);
+        root->left = createBiTree2();
+        root->right = createBiTree2();
+        return root;
     }
 }
 
 int main() {
     vector<int> v;
     for (int i = 1; i <= 20; ++i) {
-        cout << &v << " " << v.size() << " " << v.capacity() << " ";
+        cout << &v << " " << v.size() << " " << v.capacity() << ", ";
         v.push_back(i);
         cout << &v << " " << v.size() << " " << v.capacity() << " ";
         cout << endl;
     }
     v.shrink_to_fit();
-    v.resize(10);
+//    v.resize(10);
     cout << &v << " " << v.size() << " " << v.capacity() << " ";
     return 0;
 }
